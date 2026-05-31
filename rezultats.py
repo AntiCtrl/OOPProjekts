f487c4afd7253ba502914c7fe410fee7f8dfa90e
Anti-Ctrl <109760790+AntiCtrl@users.noreply.github.com>
2026-05-31 15:31:30 +0300

    izveidots rezultats.py

---
+++ rezultats.py
@@ -0,0 +1,20 @@
+class Rezultats:
+    def __init__(self, vards="", punkti=0, pareizas_atbildes=0, kludas=0, grutiba="", kategorija=""):
+        pass
+
+    def NoteiktRezultataTekstu(self, punkti):
+        pass
+
+    def GetVards(self): pass
+    def GetPunkti(self): pass
+    def GetPareizasAtbildes(self): pass
+    def GetKludas(self): pass
+    def GetGrutiba(self): pass
+    def GetKategorija(self): pass
+    def GetRezultataTeksts(self): pass
+
+    def Paradit(self):
+        pass
+
+    def __del__(self):
+        pass
