7eca7aca20396a2f8f2599059f4e5aa5924fc834
Anti-Ctrl <109760790+AntiCtrl@users.noreply.github.com>
2026-05-31 19:35:13 +0300

    updatoju jautajums.py logiku

--- jautajums.py
+++ jautajums.py
@@ -1,22 +1,39 @@
 class Jautajums:
+    # klase kas glaba info par vienu question
     def __init__(self, teksts="", atbildes=None, pareiza_atbilde="", kategorija="", grutiba=""):
-        pass
-
+        #aizsargatie dati lai atvasinatas klases var tiem pieklut
+        self._Teksts = teksts
+        if atbildes is None:
+            self._Atbildes = []
+        else:
+            self._Atbildes = atbildes
+        self._PareizaAtbilde = pareiza_atbilde
+        self._Kategorija = kategorija
+        self._Grutiba = grutiba
+        
+        
     def ParaditJautajumu(self):
-        pass
+        print(self._Teksts)
+        for i in range(len(self._Atbildes)):
+            print(str(i + 1) + ".", self._Atbildes[i])
 
+    # virtuala funkcija/ parbauda vai izvele sakrit ar pareizo atbildi
     def ParbauditAtbildi(self, atbilde):
-        pass
+        return atbilde == self._PareizaAtbilde
 
-    def GetTeksts(self): pass
-    def GetAtbildes(self): pass
-    def GetPareizaAtbilde(self): pass
-    def GetKategorija(self): pass
-    def GetGrutiba(self): pass
+        #get metodes, lai iegutu private datus
+    def GetTeksts(self): return self._Teksts
+    def GetAtbildes(self): return self._Atbildes
+    def GetPareizaAtbilde(self): return self._PareizaAtbilde
+    def GetKategorija(self): return self._Kategorija
+    def GetGrutiba(self): return self._Grutiba
+
+    # noklusejuma punkti atvasinatas klases tos velak paraksta
     def GetPunktiParPareizu(self): pass
     def GetPunktiParNepareizu(self): pass
     def __del__(self): pass
 
+    #atvasinatas klases, kuras manto visu no "Jautajums" klases
 class VieglsJautajums(Jautajums):
     def __init__(self, teksts="", atbildes=None, pareiza_atbilde="", kategorija=""):
         pass
