8da251ec59fcc9907ac2b394aba6f93a07668cc0
Anti-Ctrl <109760790+AntiCtrl@users.noreply.github.com>
2026-05-31 19:46:05 +0300

    updatos failu parvaldnieks

--- failu_parvaldnieks.py
+++ failu_parvaldnieks.py
@@ -1,18 +1,70 @@
+import json
+import csv
+from jautajums import VieglsJautajums, VidejsJautajums, GrutsJautajums
+
 class FailuParvaldnieks:
+    # klase kas atbild par visu datu read/write
     def __init__(self, jautajumu_fails="dati/jautajumi.json", rezultatu_fails="dati/rezultati.csv"):
-        pass
+        self.__JautajumuFails = jautajumu_fails
+        self.__RezultatuFails = rezultatu_fails
 
+    # nolasa jautājumus no jsona un izveido klases objektus
     def NolasitJautajumus(self, kategorija, grutiba):
-        pass
+        Fails = open(self.__JautajumuFails, "r", encoding="utf-8")
+        Teksts = Fails.read()
+        Fails.close()
 
+        Dati = json.loads(Teksts)
+        Jautajumi = []  #  saraksts 
+
+        # atlasa tikai tos jautājumus kas atbilst  kategorijai un grutibai
+        for ieraksts in Dati:
+            if ieraksts["kategorija"] == kategorija and ieraksts["grutiba"] == grutiba:
+                Teksts = ieraksts["jautajums"]
+                Atbildes = ieraksts["atbildes"]
+                Pareiza = ieraksts["pareiza"]
+
+                # izveido pareizo apaksklasi, balstoties uz grutibu // polimorfisms
+                if grutiba == "Viegls":
+                    Jautajumi.append(VieglsJautajums(Teksts, Atbildes, Pareiza, kategorija))
+                elif grutiba == "Vidējs":
+                    Jautajumi.append(VidejsJautajums(Teksts, Atbildes, Pareiza, kategorija))
+                else:
+                    Jautajumi.append(GrutsJautajums(Teksts, Atbildes, Pareiza, kategorija))
+
+        return Jautajumi
+
+    # beigu rezultats tiek pievienots csv faila 
     def SaglabatRezultatu(self, rezultats):
-        pass
+        Fails = open(self.__RezultatuFails, "a", encoding="utf-8", newline="")
+        Rakstitajs = csv.writer(Fails)
+        Rakstitajs.writerow([
+            rezultats.GetVards(), rezultats.GetPunkti(), rezultats.GetPareizasAtbildes(),
+            rezultats.GetKludas(), rezultats.GetKategorija(), rezultats.GetGrutiba(),
+            rezultats.GetRezultataTeksts()
+        ])
+        Fails.close()
 
+    # nolasa visus ieprieksejos rezultatus lai parādītu tos tabulā
     def NolasitRezultatus(self):
-        pass
+        Rezultati = []
+        try:
+            Fails = open(self.__RezultatuFails, "r", encoding="utf-8")
+            Lasitajs = csv.reader(Fails)
+            for rinda in Lasitajs:
+                Rezultati.append(rinda)
+            Fails.close()
+        except:
+            pass # ja faila nav atgriez tuksu sarakstu
+        return Rezultati
 
+    # parbauda vai csv eksiste, ja ne izveido to un ieraksta kolonnu nosaukumus
     def IzveidotRezultatuFailuJaVajag(self):
-        pass
-
-    def __del__(self):
-        pass
+        try:
+            Fails = open(self.__RezultatuFails, "r", encoding="utf-8")
+            Fails.close()
+        except:
+            Fails = open(self.__RezultatuFails, "w", encoding="utf-8", newline="")
+            Rakstitajs = csv.writer(Fails)
+            Rakstitajs.writerow(["Vards", "Punkti", "Pareizi", "Kludas", "Kategorija", "Grutiba", "Dargums"])
+            Fails.close()
