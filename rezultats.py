54de260a7233c878d28295f921b94b8416b453fa
Anti-Ctrl <109760790+AntiCtrl@users.noreply.github.com>
2026-05-31 19:52:45 +0300

    tika updatots rezultats.py

--- rezultats.py
+++ rezultats.py
@@ -1,20 +1,38 @@
+
 class Rezultats:
+     # klase kas atbild par rezultata save/apstradi
     def __init__(self, vards="", punkti=0, pareizas_atbildes=0, kludas=0, grutiba="", kategorija=""):
-        pass
-
+        # private dati 
+        self.__Vards = vards
+        self.__Punkti = punkti
+        self.__PareizasAtbildes = pareizas_atbildes
+        self.__Kludas = kludas
+        self.__Grutiba = grutiba
+        self.__Kategorija = kategorija
+        #  nosaka tekstu atkarībā no iegūtajiem punktiem
+        self.__RezultataTeksts = self.NoteiktRezultataTekstu(punkti)
+        
     def NoteiktRezultataTekstu(self, punkti):
+        if punkti >= 80:
+            return "Tu atradi dārgumu!"
+        elif punkti >= 50:
+            return "Tu biji tuvu tam, lai atrastu dārgumu!"
+        else:
+            return "Diemžēl tu neatradi dārgumu."
         pass
+    #get motodes 
+    def GetVards(self): return self.__Vards
+    def GetPunkti(self): return self.__Punkti
+    def GetPareizasAtbildes(self): return self.__PareizasAtbildes
+    def GetKludas(self): return self.__Kludas
+    def GetGrutiba(self): return self.__Grutiba
+    def GetKategorija(self): return self.__Kategorija
+    def GetRezultataTeksts(self): return self.__RezultataTeksts
 
-    def GetVards(self): pass
-    def GetPunkti(self): pass
-    def GetPareizasAtbildes(self): pass
-    def GetKludas(self): pass
-    def GetGrutiba(self): pass
-    def GetKategorija(self): pass
-    def GetRezultataTeksts(self): pass
-
+    # izdruka rez konsole testam
     def Paradit(self):
-        pass
-
-    def __del__(self):
-        pass
+        print("Spēlētājs:", self.__Vards)
+        print("Punkti:", self.__Punkti)
+        print("Pareizas atbildes:", self.__PareizasAtbildes)
+        print("Kļūdas:", self.__Kludas)
+        print("Rezultāts:", self.__RezultataTeksts)
