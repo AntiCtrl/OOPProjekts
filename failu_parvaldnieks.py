0d7bc252afcbf86cf5c9838c046d4e203d9d3f29
Anti-Ctrl <109760790+AntiCtrl@users.noreply.github.com>
2026-05-31 15:30:20 +0300

    Izveidotas klašu sagataves

---
+++ failu_parvaldnieks.py
@@ -0,0 +1,18 @@
+class FailuParvaldnieks:
+    def __init__(self, jautajumu_fails="dati/jautajumi.json", rezultatu_fails="dati/rezultati.csv"):
+        pass
+
+    def NolasitJautajumus(self, kategorija, grutiba):
+        pass
+
+    def SaglabatRezultatu(self, rezultats):
+        pass
+
+    def NolasitRezultatus(self):
+        pass
+
+    def IzveidotRezultatuFailuJaVajag(self):
+        pass
+
+    def __del__(self):
+        pass
